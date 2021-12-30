2df96d53e20903d5bc48f4368bb9b25616650aa3
pietrobelligoli <pietro.belligoli@student.h-farm.com>
2021-12-30 12:53:37 +0100

    get_balance begin

---
+++ packages/get_balance.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+
+def get_balance(): 
+    
+    df_inventory = pd.read_csv(r'csv_file/inventory.csv') 
+    df_wallet = pd.read_csv(r'csv_file/wallet.csv') 
+
+    #Inventory balance 
+    print('Inventory: \n') 
+    for i in range(0, len(df_inventory)): 
+        print(df_inventory.loc[i, 'Metal'], 'balance:', 
+              df_inventory.loc[i, 'Quantity'], 'grams. \n') 
+
+    #Cash available
+    print('Cash available:', df_wallet.loc[0, 'Balance'], 'EUR', '\n') 
+
+    #Bank loan
+    print('Debt with bank:', df_wallet.loc[0, 'Bank_Loan'], 'EUR', '\n') 
