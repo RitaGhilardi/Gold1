c5be3629b607174a1fe54fd4d701aeddce489410
pietrobelligoli <pietro.belligoli@student.h-farm.com>
2021-12-31 15:43:36 +0100

    added docstrings read_register

--- packages/read_register.py
+++ packages/read_register.py
@@ -1,16 +1,22 @@
 def read_register():
 
-    # Open the register 
-    import pandas as pd 
-    register = pd.read_csv(r'csv_file/register.csv') 
+    ''' This function can be called only by employees.
+        The function print the register of all transactiond
+        done by the company.
+    '''
 
-    print('Register of transactions: \n') 
+    #Open the register
 
-    # Print each line 
+    import pandas as pd
+    register = pd.read_csv(r'csv_file/register.csv')
+
+    print('Register of transactions: \n')
+
+    #Print each line
 
     for i in range(len(register.index)):
-        print('On', register.loc[i, 'Date'], 'the customer', 
+        print('On', register.loc[i, 'Date'], 'the customer',
               register.loc[i, 'Customer'], 'bought ',
-              register.loc[i, 'Quantity'], 'g of',  
-              register.loc[i, 'Metal'], 'at the price of', 
-              register.loc[i, 'Price'], 'euro. \n') 
+              register.loc[i, 'Quantity'], 'g of',
+              register.loc[i, 'Metal'], 'at the price of',
+              register.loc[i, 'Price'], 'euro. \n')
