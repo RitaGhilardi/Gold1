2c77510e8916059e90fe97d10f537180b427ac3f
pietrobelligoli <pietro.belligoli@student.h-farm.com>
2022-01-02 10:51:09 +0100

    created pay_loan module

---
+++ packages/pay_loan.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+def pay_loan():
+    
+    w = pd.read_csv(r'csv_file/wallet.csv')
+
+    #Control if there are bank loan to be repaid
+
+    if w.loc[0, 'Bank_Loan'] == 0:
+
+        print('There are no debts to be repaid')
+
+    elif w.loc[0, 'Balance'] == 0:
+
+        print('You have no money to repay your bank loan')
+
+    else:
+
+        if w.loc[0, 'Bank_Loan'] <= w.loc[0, 'Balance']:
+
+            #Pay all the bank loan
+            amount = w.loc[0, 'Bank_Loan']
+            w.loc[0, 'Balance'] = w.loc[0, 'Balance'] - amount
+            w.loc[0, 'Bank_Loan'] = 0
+            print('You have just paid', amount,
+                  'EUR to the bank and now you have no more debts')
+
+        else:
+
+            #Gives all the money we have to repay part of the bank loan
+
+            amount = w.loc[0, 'Balance']
+            w.loc[0, 'Bank_Loan'] = w.loc[0, 'Bank_Loan'] - amount
+            w.loc[0, 'Balance'] = 0
+
+            print('You have just paid', amount, 'EUR to the bank,',
+                  'but you still have a debt of',
+                  w.loc[0, 'Bank_Loan'], 'EUR')
+
+        w.to_csv(r'csv_file/wallet.csv', index=False)
