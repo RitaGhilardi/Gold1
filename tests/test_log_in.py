6b05d5f21e154a066c3ece21dbd343a970579998
Rithoshi <ghilardi.rita@hotmail.it>
2022-01-05 13:10:59 +0100

    added test_log_in

--- tests/test_log_in.py
+++ tests/test_log_in.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 import unittest
 import sys
 import os
@@ -11,28 +12,29 @@ class Test(unittest.TestCase):
     
     def setUp(self):
 
-        # user log_in
+        # valid inputs
         self.email_user = "roberto.celi@gmail.com"
         self.password_user = "Peru2021"
-        self.f_email_user = "roberto"
-        self.f_password_user = "Italy"
-
-        # employee log_in
         self.email_empl = "rita.ghilardi@gold1.com"
         self.password_empl = "HeidieMilady2"
+        
+        # invalid inputs
+        self.f_email_user = "roberto"
+        self.f_password_user = "Italy"
         self.f_email_empl = ""
         self.f_password_empl = "Heidi1"
 
 
-    def test_log_in(self):
+    def test_valid_inputs(self):
         self.assertTrue(log_in(self.email_user, self.password_user))
+        self.assertTrue(log_in(self.email_empl, self.password_empl))
+        
+    def test_invalid_inputs(self):
         self.assertFalse(log_in(self.email_user, self.f_password_user))
         self.assertFalse(log_in(self.f_email_user, self.password_user))
-        self.assertTrue(log_in(self.email_empl, self.password_empl))
         self.assertFalse(log_in(self.email_empl, self.f_password_empl))
         self.assertFalse(log_in(self.f_email_empl, self.password_empl))
 
     
-
 if __name__ == "__main__":
     unittest.main()
