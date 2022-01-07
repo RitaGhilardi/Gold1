2489f51dcd4eaa3675f3c69a8c3c72c88d051d77
AlessandroBonetto <alessandro.bonetto@student.h-farm.com>
2022-01-07 18:15:28 +0100

    pep8 completed in test_verify_user

--- tests/test_verify_user.py
+++ tests/test_verify_user.py
@@ -8,14 +8,15 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from packages.verify_user import verify_user
 
+
 class Test(unittest.TestCase):
     def setUp(self):
-    
+
         # valid inputs
         self.user1 = "eva.castelli@gmail.com"
         self.user2 = "giacomo@hotmail.it"
         self.user3 = "pietro.belligoli@student.h-farm.com"
-        
+
         # invalid inputs
         self.f_mail1 = "roberto.celi@gmail.com"
         self.f_mail2 = "luca@gold1.com"
@@ -23,12 +24,12 @@ class Test(unittest.TestCase):
         self.f_mail4 = ""
         self.f_mail5 = "ciao"
         self.f_mail6 = "anna@me"
-        
+
     def test_valid_inputs(self):
         self.assertTrue(verify_user(self.user1))
         self.assertTrue(verify_user(self.user2))
         self.assertTrue(verify_user(self.user3))
-        
+
     def test_invalid_inputs(self):
         self.assertFalse(verify_user(self.f_mail1))
         self.assertFalse(verify_user(self.f_mail2))
@@ -36,6 +37,7 @@ class Test(unittest.TestCase):
         self.assertFalse(verify_user(self.f_mail4))
         self.assertFalse(verify_user(self.f_mail5))
         self.assertFalse(verify_user(self.f_mail6))
-        
+
+
 if __name__ == "__main__":
     unittest.main()
