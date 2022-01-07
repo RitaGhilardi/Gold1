b16a90da1f0ae084d6c04deeffe71aefa1f4e102
francescasignorello <francesca.signorello@student.h-farm.com>
2022-01-07 10:57:25 +0100

    pep8 check

--- packages/add_employee.py
+++ packages/add_employee.py
@@ -5,70 +5,66 @@ from email_validator import validate_email, EmailNotValidError
 
 from packages.check_password import check_password
 
+
 def add_employee(email, password):
-    
     df_employees = pd.read_csv(r'csv_files/employees.csv')
     db_employees = pd.read_csv(r'csv_files/db_employees.csv')
-    
-    #Check if the mail is valid format
-    
+
+# Check if the mail is valid format
+
     result = False
 
-    try: 
+    try:
         valid = validate_email(email)
         email = valid.email
-        
-        #Check if the domain is the correct one
-        
+
+        # Check if the domain is the correct one
         if "@gold1.com" not in email:
             print("Please enter an employee email. \n")
 
         else:
             check = False
-            
-            #Check if the employee is already registered
-            
+            # Check if the employee is already registered
             for mail in db_employees["email"]:
                 if mail == email:
                     check = True
                     print("This account is already registered. \n")
                     break
 
-            #Check if the email is in the one allowed to register as an employee
-            
-            if check == False:     
+            # Check if the email is in the one allowed
+            # to register as an employee
+            if check is False:
                 presence = False
                 for mail in df_employees["email"]:
                     if email == mail:
                         presence = True
-                        print('Your email allows you to register as an employee. \n')
-                        
-                        #Ask the employee to confirm the password he want to use
-                        
+                        print('Your email allows you to'
+                              'register as an employee. \n')
+
+                        # Ask the employee to confirm the password
+                        # he want to use
                         password_check = check_password(password)
-                        if password_check == True:
-                            
-                            #Register the employee
-                            
-                            digest_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-                            new_df = pd.DataFrame({"email": [email], "password": [digest_password]})
+                        if password_check is True:
+
+                            # Register the employee
+                            digest_password = hashlib.sha256(
+                                password.encode('utf-8')).hexdigest()
+                            new_df = pd.DataFrame(
+                                {"email": [email],
+                                    "password": [digest_password]})
                             db_employees = db_employees.append(new_df)
-                            db_employees.to_csv(r'csv_files/db_employees.csv', index = False)
+                            db_employees.to_csv(r'csv_files/db_employees.csv',
+                                                index=False)
                             result = True
-                            print("Registration was successful!. \n")
-                            
-                        
-                        break
-                        
+                        print("Registration was successful!. \n")
+                    break
 
-                if presence == False: 
-                    print("We are sorry, this email is not allowed to register as an employee. \n")
-        
+                if presence is False:
+                    print("We are sorry, this email is not allowed"
+                          "to register as an employee. \n")
 
-                    
     except EmailNotValidError as e:
-    
+
         print(str(e))
-    
-    return result                   
-    
+
+    return result
