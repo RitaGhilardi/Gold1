1d821942bd3ca5740dd6acc1c6ca9f59f2010205
AlessandroBonetto <alessandro.bonetto@student.h-farm.com>
2022-01-07 18:11:24 +0100

    pep8 completed on verify_user

--- packages/verify_user.py
+++ packages/verify_user.py
@@ -5,27 +5,28 @@ import random
 from email_validator import validate_email
 from email_validator import EmailNotValidError
 
+
 def verify_user(email):
 
-    ''' This function controls that the email of the new user is in 
-        the right format, is not already registered and is not a mail
-        of an employee.    
+    '''This function controls that the email of the new user is in
+       the right format, is not already registered and is not a mail
+       of an employee.
     '''
-    
+
     result = False
-    
+
     if type(email) is not str:
-        
-        #Something wrong happened
-        
+
+        # Something wrong happened
+
         print('ERROR: one of the input is in an unexpected type.',
-            'Please contact the customer service to notify the error. \n')
-    
+              'Please contact the customer service to notify the error. \n')
+
     else:
-        
-        #The input are of the correct type
-        
-        db_users = pd.read_csv(r'csv_files/db_users.csv')    
+
+        # The input are of the correct type
+
+        db_users = pd.read_csv(r'csv_files/db_users.csv')
 
         if "@" not in email:
             print("Please enter a valid email. \n")
@@ -54,7 +55,7 @@ def verify_user(email):
                     if presence is False:
                         result = True
                         print('Your email was accepted. \n')
-                            
+
                 except EmailNotValidError as e:
                     print(str(e))
 
