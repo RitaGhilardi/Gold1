66ff8a7b54b73d34c2fe7b30ff9862b2e128eb24
Marco Visentin <marco.visentin@student.h-farm.com>
2022-01-07 21:50:20 +0100

    get_prices done

--- packages/get_prices.py
+++ packages/get_prices.py
@@ -1,23 +1,23 @@
 def get_prices():
     import requests
 
-    ''' This function get called by the function "buy_metals" when in the 
+    ''' This function get called by the function "buy_metals" when in the
         inventory there is not enough of some metals. The function calls and
         API to get all the current prices of metals and modify them go have
-        price per grams. The function return a dictionary with the current 
-        prices and a boolean variable that state if the API was online and 
+        price per grams. The function return a dictionary with the current
+        prices and a boolean variable that state if the API was online and
         so if the dictionary is full of trustable values.
     '''
-    
+
     # Create the correct link
-    
+
     symbol = 'XAU,XAG,XPD,XPT,XRH'
     base_currency = 'EUR'
     endpoint = 'latest'
     access_key = 'o65ww2g1zd18fqm7euy9h2at766a9amft1r6srflwfle1gqu6lx029fshbfu'
 
-    #request to the API
-    
+    # Request to the API
+
     resp = requests.get('https://metals-api.com/api/' + endpoint
                         + '?access_key=' + access_key + '&base='
                         + base_currency + '&symbols=' + symbol)
@@ -30,8 +30,8 @@ def get_prices():
 
         rates = resp.json()['rates']
 
-        #Convert values in Euro over grams and add more readable names
-        
+        # Convert values in Euro over grams and add more readable names
+
         oz = 28.3495
 
         conversion = {'XAU': 'Gold',
